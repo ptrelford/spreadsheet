2abddbdc08f14dccef74458c581fc4ca87181973
ptrelford <ptrelford@localhost>
2011-04-30 14:38:08 +0100

    Added support for ranges, logical operators and IF function.

--- Spreadsheet.fs
+++ Spreadsheet.fs
@@ -12,32 +12,30 @@ open System.Windows.Data
 open System.Windows.Input
 open System.Windows.Media
 
-let rec toRef (s:string) =
-    let col = int s.[0] - int 'A'
-    let row = s.Substring 1 |> int
-    col, row-1
-
 type token =
     | WhiteSpace
     | Symbol of char
     | OpToken of string
     | RefToken of int * int
-    | FunToken of string
+    | StrToken of string
     | NumToken of decimal 
 
 let (|Match|_|) pattern input =
     let m = System.Text.RegularExpressions.Regex.Match(input, pattern)
     if m.Success then Some m.Value else None
 
+let rec toRef (s:string) =
+    let col = int s.[0] - int 'A'
+    let row = s.Substring 1 |> int
+    col, row-1
+
 let matchToken = function
     | Match @"^\s+" s -> s, WhiteSpace
-    | Match @"^\+"  s -> s, OpToken s
-    | Match @"^\-"  s -> s, OpToken s
-    | Match @"^\*"  s -> s, OpToken s
-    | Match @"^\/"  s -> s, OpToken s
-    | Match @"^\(|^\)|^\," s -> s, Symbol s.[0]   
+    | Match @"^\+|^\-|^\*|^\/"  s -> s, OpToken s
+    | Match @"^=|^<>|^<=|^>=|^>|^<"  s -> s, OpToken s   
+    | Match @"^\(|^\)|^\,|^\:" s -> s, Symbol s.[0]   
     | Match @"^[A-Z]\d+" s -> s, s |> toRef |> RefToken
-    | Match @"^[A-Za-z]+" s -> s, FunToken s
+    | Match @"^[A-Za-z]+" s -> s, StrToken s
     | Match @"^\d+(\.\d+)?|\.\d+" s -> s, s |> decimal |> NumToken
     | _ -> invalidOp ""
 
@@ -51,78 +49,105 @@ let tokenize s =
     tokenize' 0 s
     |> List.choose (function WhiteSpace -> None | t -> Some t)
 
-type operator = Add | Sub | Mul | Div
-
-type expr =
-    | Neg of expr
-    | Op of expr * operator * expr
+type arithmeticOp = Add | Sub | Mul | Div
+type logicalOp = Eq | Lt | Gt | Le | Ge | Ne
+type formula =
+    | Neg of formula
+    | ArithmeticOp of formula * arithmeticOp * formula
+    | LogicalOp of formula * logicalOp * formula
     | Num of decimal
     | Ref of int * int
-    | Fun of string * expr list
+    | Range of int * int * int * int
+    | Fun of string * formula list
 
-let rec (|Term|_|) = function  
-    | Factor(e1, t) ->      
-        let rec aux e1 = function        
-            | Sum op::Factor(e2, t) -> aux (Op(e1,op,e2)) t               
-            | t -> Some(e1, t)      
-        aux e1 t  
+let rec (|Term|_|) = function
+    | Sum(f1, (OpToken(LogicOp op))::Sum(f2,t)) -> Some(LogicalOp(f1,op,f2),t)
+    | Sum(f1,t) -> Some (f1,t)
     | _ -> None
-and (|Sum|_|) = function 
-    | OpToken "+" -> Some Add 
-    | OpToken "-" -> Some Sub 
+and (|LogicOp|_|) = function
+    | "=" ->  Some Eq | "<>" -> Some Ne
+    | "<" ->  Some Lt | ">"  -> Some Gt
+    | "<=" -> Some Le | ">=" -> Some Ge
+    | _ -> None
+and (|Sum|_|) = function
+    | Factor(f1, t) ->      
+        let rec aux f1 = function        
+            | SumOp op::Factor(f2, t) -> aux (ArithmeticOp(f1,op,f2)) t               
+            | t -> Some(f1, t)      
+        aux f1 t  
+    | _ -> None
+and (|SumOp|_|) = function 
+    | OpToken "+" -> Some Add | OpToken "-" -> Some Sub 
     | _ -> None
 and (|Factor|_|) = function  
-    | OpToken "-"::Factor(e, t) -> Some(Neg(e), t)  
-    | Atom(e1, Product op::Factor(e2, t)) ->
-        Some(Op(e1,op,e2), t)       
-    | Atom(e, t) -> Some(e, t)  
+    | OpToken "-"::Factor(f, t) -> Some(Neg f, t)
+    | Atom(f1, ProductOp op::Factor(f2, t)) ->
+        Some(ArithmeticOp(f1,op,f2), t)       
+    | Atom(f, t) -> Some(f, t)  
     | _ -> None    
-and (|Product|_|) = function
-    | OpToken "*" -> Some Mul
-    | OpToken "/" -> Some Div
+and (|ProductOp|_|) = function
+    | OpToken "*" -> Some Mul | OpToken "/" -> Some Div
     | _ -> None
 and (|Atom|_|) = function      
-    | NumToken(n)::t -> Some(Num(n), t)   
+    | NumToken n::t -> Some(Num n, t)
+    | RefToken(x1,y1)::(Symbol ':'::RefToken(x2,y2)::t) -> 
+        Some(Range(min x1 x2,min y1 y2,max x1 x2,max y1 y2),t)  
     | RefToken(x,y)::t -> Some(Ref(x,y), t)
-    | Symbol '('::Term(e, Symbol ')'::t) -> Some(e, t)
-    | FunToken(s)::Tuple(ps, t) -> Some(Fun(s,ps),t)  
+    | Symbol '('::Term(f, Symbol ')'::t) -> Some(f, t)
+    | StrToken s::Tuple(ps, t) -> Some(Fun(s,ps),t)  
     | _ -> None
 and (|Tuple|_|) = function
     | Symbol '('::Params(ps, Symbol ')'::t) -> Some(ps, t)  
     | _ -> None
 and (|Params|_|) = function
-    | Term(e1, t) ->
-        let rec aux es = function
-            | Symbol ','::Term(e2, t) -> aux (es@[e2]) t
-            | t -> es, t
-        Some(aux [e1] t)
+    | Term(f1, t) ->
+        let rec aux fs = function
+            | Symbol ','::Term(f2, t) -> aux (fs@[f2]) t
+            | t -> fs, t
+        Some(aux [f1] t)
     | t -> Some ([],t)
 
 let parse s = 
     tokenize s |> function 
-    | Term(e,[]) -> e 
+    | Term(f,[]) -> f 
     | _ -> failwith "Failed to parse formula"
 
-let evaluate valueAt s =
+let evaluate valueAt formula =
     let rec eval = function
-        | Neg e -> - (eval e)
-        | Op(e1,Add,e2) -> eval e1 + eval e2
-        | Op(e1,Sub,e2) -> eval e1 - eval e2
-        | Op(e1,Mul,e2) -> eval e1 * eval e2
-        | Op(e1,Div,e2) -> eval e1 / eval e2
+        | Neg f -> - (eval f)
+        | ArithmeticOp(f1,op,f2) -> arithmetic op (eval f1) (eval f2)
+        | LogicalOp(f1,op,f2) -> if logic op (eval f1) (eval f2) then 0.0M else -1.0M
         | Num d -> d
         | Ref(x,y) -> valueAt(x,y) |> decimal
-        | Fun("SUM",ps) -> ps |> List.map eval |> List.sum
+        | Range _ -> invalidOp "Expected in function"
+        | Fun("SUM",ps) -> ps |> evalAll |> List.sum
+        | Fun("IF",[condition;f1;f2]) -> 
+            if (eval condition)=0.0M then eval f1 else eval f2 
         | Fun(_,_) -> failwith "Unknown function"
-    eval s
+    and arithmetic = function
+        | Add -> (+) | Sub -> (-) | Mul -> (*) | Div -> (/)
+    and logic = function         
+        | Eq -> (=)  | Ne -> (<>)
+        | Lt -> (<)  | Gt -> (>)
+        | Le -> (<=) | Ge -> (>=)
+    and evalAll ps =
+        ps |> List.collect (function            
+            | Range(x1,y1,x2,y2) ->
+                [for x=x1 to x2 do for y=y1 to y2 do yield valueAt(x,y) |> decimal]
+            | x -> [eval x]            
+        )
+    eval formula
 
-let references s =
+let references formula =
     let rec traverse = function
         | Ref(x,y) -> [x,y]
+        | Range(x1,y1,x2,y2) -> 
+            [for x=x1 to x2 do for y=y1 to y2 do yield x,y]
         | Fun(_,ps) -> ps |> List.collect traverse
-        | Op(e1,_,e2) -> traverse e1 @ traverse e2
+        | ArithmeticOp(f1,_,f2) | LogicalOp(f1,_,f2) -> 
+            traverse f1 @ traverse f2
         | _ -> []
-    traverse s
+    traverse formula
 
 type Sheet (colCount,rowCount) as sheet =
     let cols = Array.init colCount (fun i -> string (int 'A' + i |> char)) 
@@ -138,7 +163,7 @@ and Cell (sheet:Sheet) as cell =
     inherit ObservableObject()
     let mutable value = ""
     let mutable data = ""       
-    let mutable expr : expr option = None
+    let mutable formula : formula option = None
     let updated = Event<_>()
     let mutable subscriptions : System.IDisposable list = []   
     let cellAt(x,y) = 
@@ -146,8 +171,8 @@ and Cell (sheet:Sheet) as cell =
         let (cell : Cell) = Array.get row.Cells x
         cell
     let valueAt address = (cellAt address).Value
-    let eval expr =         
-        try (evaluate valueAt expr).ToString()       
+    let eval formula =         
+        try (evaluate valueAt formula).ToString()       
         with _ -> "N/A"
     let parseFormula (text:string) =
         if text.StartsWith "="
@@ -163,14 +188,14 @@ and Cell (sheet:Sheet) as cell =
     let unsubscribe () =
         subscriptions |> List.iter (fun d -> d.Dispose())
         subscriptions <- []
-    let subscribe expr addresses =
+    let subscribe formula addresses =
         let remember x = subscriptions <- x :: subscriptions
         for address in addresses do
             let cell' : Cell = cellAt address
             cell'.Updated
             |> Observable.subscribe (fun generation ->   
                 if generation < sheet.MaxGeneration then
-                    let newValue = eval expr
+                    let newValue = eval formula
                     update newValue (generation+1)
             ) |> remember
     member cell.Data 
@@ -178,13 +203,13 @@ and Cell (sheet:Sheet) as cell =
         and set (text:string) =
             data <- text        
             cell.Notify "Data"          
-            let isFormula, newExpr = parseFormula text               
-            expr <- newExpr
+            let isFormula, newFormula = parseFormula text               
+            formula <- newFormula
             unsubscribe()
-            expr |> Option.iter (fun expr ->  references expr |> subscribe expr)
+            formula |> Option.iter (fun f -> references f |> subscribe f)
             let newValue =
-                match isFormula, expr with           
-                | _, Some expr -> eval expr
+                match isFormula, formula with           
+                | _, Some f -> eval f
                 | true, _ -> "N/A"
                 | _, None -> text
             update newValue 0
@@ -199,16 +224,15 @@ and ObservableObject() =
         [<CLIEvent>]
         member this.PropertyChanged = propertyChanged.Publish
 
+let (+.) (grid:Grid) (child:#UIElement) = grid.Children.Add child |> ignore; grid
+
 type DataGrid(headings:seq<_>, items:IEnumerable, cellFactory:int*int->FrameworkElement) as grid =
-    inherit Grid()    
-    do  grid.ShowGridLines <- true   
+    inherit Grid(ShowGridLines=true)    
     let createHeader heading horizontalAlignment =
         let header = TextBlock(Text=heading)
         header.HorizontalAlignment <- horizontalAlignment
         header.VerticalAlignment <- VerticalAlignment.Center
-        let container = Grid(Background=SolidColorBrush Colors.Gray)        
-        container.Children.Add header |> ignore
-        container
+        Grid(Background=SolidColorBrush Colors.Gray) +. header
     do  ColumnDefinition(Width=GridLength(24.0)) |> grid.ColumnDefinitions.Add 
     do  headings |> Seq.iteri (fun i heading ->
         let width = GridLength(64.0)
@@ -222,7 +246,7 @@ type DataGrid(headings:seq<_>, items:IEnumerable, cellFactory:int*int->Framework
         let mutable y = 1
         for item in items do
         RowDefinition(Height=height) |> grid.RowDefinitions.Add
-        let header = createHeader (y.ToString()) HorizontalAlignment.Right       
+        let header = createHeader (y.ToString()) HorizontalAlignment.Right
         grid.Children.Add header |> ignore
         Grid.SetRow(header,y)
         for x=1 to Seq.length headings do
@@ -236,19 +260,18 @@ type DataGrid(headings:seq<_>, items:IEnumerable, cellFactory:int*int->Framework
 type View() =
     inherit UserControl()  
     let sheet = Sheet(26,50)
-    let remember = ignore
     let cellFactory (x,y) =
         let binding = Binding(sprintf "Cells.[%d].Data" x)   
         binding.Mode <- BindingMode.TwoWay
         let edit = TextBox()
         edit.SetBinding(TextBox.TextProperty,binding) |> ignore        
         edit.Visibility <- Visibility.Collapsed
-        let view = Button(Background=SolidColorBrush Colors.White)
-        view.BorderBrush <- null
-        let binding = Binding(sprintf "Cells.[%d].Value" x)
         let block = TextBlock()
+        let binding = Binding(sprintf "Cells.[%d].Value" x)
         block.SetBinding(TextBlock.TextProperty, binding) |> ignore
-        view.Content <- block
+        let view = Button(Content=block)
+        view.Background <- SolidColorBrush Colors.White
+        view.BorderBrush <- null        
         view.HorizontalContentAlignment <- HorizontalAlignment.Left
         view.VerticalContentAlignment <- VerticalAlignment.Center
         let setEditMode _ =
@@ -258,14 +281,11 @@ type View() =
         let setViewMode _ =
             edit.Visibility <- Visibility.Collapsed
             view.Visibility <- Visibility.Visible        
-        view.Click |> Observable.subscribe setEditMode |> remember
-        edit.LostFocus |> Observable.subscribe setViewMode |> remember        
+        view.Click |> Observable.subscribe setEditMode |> ignore
+        edit.LostFocus |> Observable.subscribe setViewMode |> ignore
         let enterKeyDown = edit.KeyDown |> Observable.filter (fun e -> e.Key = Key.Enter)
-        enterKeyDown |> Observable.subscribe setViewMode |> remember
-        let container = Grid()
-        container.Children.Add view |> ignore
-        container.Children.Add edit |> ignore
-        container :> FrameworkElement
+        enterKeyDown |> Observable.subscribe setViewMode |> ignore
+        Grid() +. view +. edit :> FrameworkElement
     let viewer = ScrollViewer(HorizontalScrollBarVisibility=ScrollBarVisibility.Auto)
     do  viewer.Content <- DataGrid(sheet.Columns,sheet.Rows,cellFactory)
     do  base.Content <- viewer
